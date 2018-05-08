0066f2f3452b458d9d97401b4ae596371d023def
lizzie marie <elizabethmariewillett@gmail.com>
2018-05-07 21:03:35 -0700

    my first commit

---
+++ front-page.php
@@ -0,0 +1,239 @@
+<?php
+/**
+ * The template for displaying the front page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ *
+ * @package bst
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		    <div class="intro-section">
+          <div id="nav-icon">
+            <a href="#">
+              <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/ham-nav.png" alt="Nav Menu">
+            </a>
+          </div>
+
+          <div class="bs-welcome">
+            <h1>Bandana Studio</h1>
+            <p>a design and web development studio for creative entrepreneurs</p>
+            <p class="hide">let's build the website of your dreams</p>
+            <h4>Welcome</h4>
+          </div>
+
+          <div id="scroll-triangle">
+            <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/scroll-triangle.png" alt="keep scrolling" height="40">
+          </div>
+
+        </div> <!-- .intro-section -->
+
+        <div class="about-section">
+          <div class="studio-about">
+            <div class="studio-about-item-1">
+              <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/Screen-Shot-2018-05-07-at-1.25.17-PM.png" alt="keep scrolling" height="316" width="339">
+            </div>
+
+            <div class="studio-about-item-a">
+              <h5>B</h5>
+            </div>
+
+            <div class="studio-about-item-2">
+              <h2 class="text-shadow underline">It's time</h2>
+              <h2 id="no-mt" class="text-shadow underline">to shine</h2>
+              <h3>Let's build your dream site</h3>
+            </div>
+
+            <div class="studio-about-item-3">
+              <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/Screen-Shot-2018-05-07-at-1.25.17-PM.png" alt="keep scrolling" height="316" width="339">
+            </div>
+
+            <div class="studio-about-item-4">
+              <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/Screen-Shot-2018-05-07-at-1.25.17-PM.png" alt="keep scrolling" height="316" width="339">
+            </div>
+
+            <div class="studio-about-item-b">
+              <h5>*</h5>
+            </div>
+
+            <div class="studio-about-item-5">
+              <h3>Let's build your dream site</h3>
+              <h6>Website Design</h6>
+              <p>Let’s build a dream website for you and your business.  A stunning website is the cornerstone to any thriving idea and I will design and build one that you’re finally proud of.</p>
+              <p>We will be creative partners in building a stylish, functional, unique site that meets your business needs.  I’ll use your vision as the framework for design concepts to development, revisions, testing, launch and post launch support.</p>
+            </div>
+
+            <div class="studio-about-item-6">
+              <p>
+                <a class="btn pill" href="#">get started</a>
+              </p>
+            </div>
+
+          </div>
+
+          <div class="h-i-w">
+            <h6>How it works</h6>
+            <ul>
+              <li class="hiw-li">
+                <div class="hiw-i-1">
+                  <h8>*</h8>
+                </div>
+                <div class="hiw-i-2">
+                  <h3>Inquiry</h3>
+                </div>
+                <div class="hiw-i-3">
+                  <p>let’s talk shop! tell me about your business. what are your project goals?</p>
+                </div>
+              </li>
+
+              <li class="hiw-li">
+                <div class="hiw-i-1">
+                  <h8>*</h8>
+                </div>
+                <div class="hiw-i-2">
+                  <h3>Exploration</h3>
+                </div>
+                <div class="hiw-i-3">
+                  <p>visual inspirations, vibes and overall brand feelings.</p>
+                </div>
+              </li>
+
+              <li class="hiw-li">
+                <div class="hiw-i-1">
+                  <h8>*</h8>
+                </div>
+                <div class="hiw-i-2">
+                  <h3>Design</h3>
+                </div>
+                <div class="hiw-i-3">
+                  <p>i’ll jump into creating moodboards and design concepts for your review.</p>
+                </div>
+              </li>
+
+              <li class="hiw-li">
+                <div class="hiw-i-1">
+                  <h8>*</h8>
+                </div>
+                <div class="hiw-i-2">
+                  <h3>Development</h3>
+                </div>
+                <div class="hiw-i-3">
+                  <p>development of your custom site begins!</p>
+                </div>
+              </li>
+
+              <li class="hiw-li">
+                <div class="hiw-i-1">
+                  <h8>*</h8>
+                </div>
+                <div class="hiw-i-2">
+                  <h3>Revise, Test, Launch</h3>
+                </div>
+                <div class="hiw-i-3">
+                  <p>demo site is reviewed by you, tested by me, and officially launched.</p>
+                </div>
+              </li>
+
+              <li class="hiw-li">
+                <div class="hiw-i-1">
+                  <h8>*</h8>
+                </div>
+                <div class="hiw-i-2">
+                  <h3>Post Launch Support</h3>
+                </div>
+                <div class="hiw-i-3">
+                  <p>let’s keep your site in perfect shape over time - maintenance, back-ups, etc.</p>
+                </div>
+              </li>
+
+            </ul>
+          </div> <!--  .h-i-w -->
+
+          <div>
+            <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/Screen-Shot-2018-05-07-at-1.25.17-PM.png" alt="keep scrolling" height="316" width="339">
+            <h5>*</h5>
+            <btn class="button" type="button">view all work</btn>
+          </div>
+
+          <div class="values">
+            <h6>Values</h6>
+
+            <div class="value-item">
+                <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/one-stop-shop.png" alt="one stop shop icon">
+                <h3>One-Stop Shop</h3>
+                <p>As a designer and developer I will take your website from vision to launch without the need of third parties.</p>
+            </div>
+
+            <div class="value-item">
+                <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/hassel-free-icon-–-2.png" alt="hassel free icon">
+                <h3>Hassel Free</h3>
+                <p>Through a clear step by step process, I take the technical trickiness out of designing, building and launching a website.</p>
+            </div>
+
+            <div class="value-item">
+                <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/design-icon-–-2.png" alt="design icon">
+                <h3>Stunning Design</h3>
+                <p>Let’s bring your unique vision to life through color, graphics, and responsive pixel perfect design.</p>
+            </div>
+
+
+            <btn class="button" type="button">more info</btn>
+
+          </div> <!-- .values -->
+
+          <div class="reviews">
+
+            <div class="review-box">
+              <h6>Reviews</h6>
+            </div>
+
+            <div class="review-box">
+              <div class="review-int">
+                <p>“Let’s build a dream website for you. We will be partners in buiilding a gorgeous, functional, operational, creative, unique site that meets your business needs. Let’s build a dream website for you. We will be partners."</p>
+                <h9> - Susie<br> Business Owner</h9>
+              </div>
+            </div>
+
+            <div class="review-box">
+              <p>insert bandana here </p>
+            </div>
+
+            <div class="review-box">
+              <div class="review-int">
+                <p>“We loved working with Lizzie and Bandana Studio. Our site is more than anything we could have imagined. ”</p>
+                <h9> - Susie<br> Business Owner</h9>
+              </div>
+            </div>
+
+            <div class="review-box">
+              <p>insert bandana here </p>
+            </div>
+
+            <div class="review-box">
+              <div class="review-int">
+                <p>“We loved working with Lizzie and Bandana Studio. Our site is more than anything we could have imagined. ”</p>
+                <h9> - Susie<br> Business Owner</h9>
+              </div>
+            </div>
+
+            <btn class="button" type="button">get started</btn>
+
+          </div> <!-- .reviews -->
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
