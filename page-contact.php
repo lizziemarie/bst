a1d468b7c443ca5d23e34d0070cdf4aa320cd91d
lizzie marie <elizabethmariewillett@gmail.com>
2018-05-16 15:42:07 -0700

    added contact and get started design page

---
+++ page-contact.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the contact page
+ *
+ *
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ *
+ * @package bst
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main contact-main">
+
+      <div class="contact">
+        <h6>Contact</h6>
+        <p> I can't wait to hear from you! Please fill out the form and I'll be in touch.
+        <br> <br>Email: <em>hello@bandanastudio.co</em></p>
+
+      </div> <!-- .contact-->
+      <div id="break">
+      </div>
+
+	 </main><!-- #main -->
+ </div><!-- #primary -->
+
+
+
+<?php
+get_footer();
---
+++ page-startdesign.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * The template for displaying the get started with design page
+ *
+ *
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ *
+ * @package bst
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main get-started-main">
+
+    <div class="get-started">
+
+      <div class="gs-intro">
+
+              <div class="gs-intro-item-1">
+                <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/ipad-test-bs.png" alt="keep scrolling" class="rotateimg100">
+              </div>
+
+              <div class="gs-intro-item-a">
+                <h5>B</h5>
+              </div>
+
+              <div class="gs-intro-item-2">
+                <h3>Let's build your dream site</h3>
+                <h2 class="text-shadow underline">It's time<br></h2>
+                <h2 id="no-mt" class="text-shadow underline">to shine</h2>
+                <p>With a custom WordPress site, your small business can go from “Meh,” to “Money!” as it attracts your ideal audience, blows up conversion rates, and skyrockets traffic. We take your simple seed or idea and breathe functionality, practicality and optimization through it so you can run forward with a truly inspired new site. Including full site installation, setup, and support, the investment of $2-5K is well worth it.</p>
+              </div>
+
+      </div> <!-- .gs-intro -->
+
+      <div class="gs-included">
+              <div class="gs-included-item-a">
+                <h5>*</h5>
+              </div>
+
+              <div class="gs-included-item-2">
+                <h3>Design & Development</h3>
+                <h6> What's Included </h6>
+                <ul>
+                  <li><strong>Design</strong></li>
+                  <li>Moodboard</li>
+                  <li>Design Mockups of all website pages in 3 responsive design sizes (desktop, tablet, mobile)</li>
+                  <li>2 rounds of design revisions</li>
+                </ul>
+                <ul>
+                  <li><strong>Development</strong></li>
+                  <li>Installation of the WordPress Content Management System</li>
+                  <li>Installation of theme</li>
+                  <li>Development of premium theme or custom theme depending on your business needs</li>
+                  <li>Installation and configuration of plugins relative to your theme</li>
+                </ul>
+                <ul>
+                  <li><strong>Communication</strong></li>
+                  <li>Consultation, proposal, and on-boarding</li>
+                  <li>Questionnaires and pinterest inspiration boards</li>
+                  <li>File review and deliivery</li>
+                  <li>Development notes and revisions</li>
+                  <li>Communication before, during and after site installation</li>
+                  <li>Wordpress training specific to new site via video</li>
+                </ul>
+              </div>
+
+              <div class="gs-included-item-3">
+                <p>
+                  <a class="btn pill" href="/bandana-studio/about/">view all work</a>
+                </p>
+              </div>
+      </div> <!-- .gs-included -->
+
+      <div class="gs-process">
+              <h6> The Process </h6>
+                <ul>
+  	              <li class="process-li">
+  	                <div class="process-i-1" id="ast-decor-lg">
+  	                  *
+  	                </div>
+  	                <div class="process-i-2">
+  	                  <h3>Inquiry</h3>
+  	                </div>
+  	                <div class="process-i-3">
+  	                  <p>Fill out the contact form below so we can schedule an introductory call and dig into what you’re looking for. We’ll ask you a few introductory questions to assess your needs while we look at our schedules to see if we’re a fit! If for some reason we aren’t, we’re more than happy to refer you to a few other developers we love. (If you’re a graphic designer looking to possibly collaborate, visit this page for more details.)</p>
+  	                </div>
+  	              </li>
+
+                  <li class="process-li">
+  	                <div class="process-i-1" id="ast-decor-lg">
+  	                  *
+  	                </div>
+  	                <div class="process-i-2">
+  	                  <h3>Exploration</h3>
+  	                </div>
+  	                <div class="process-i-3">
+  	                  <p>With official paperwork out of our hair, we can get into the fun, creative stuff! You’ll work through my client questionnaire (10 questions, so nothing super long!) that digs into the nitty gritty of your business goals, ideal customer and aesthetic. You’ll also craft up a private Pinterest board of 25-35 images – logos, typography, outfits, textures, colour palettes, websites – that really speak to your vibe and aspired brand feel. We’ll walk through both one week before our project start date.</p>
+  	                </div>
+  	              </li>
+
+                  <li class="process-li">
+  	                <div class="process-i-1" id="ast-decor-lg">
+  	                  *
+  	                </div>
+  	                <div class="process-i-2">
+  	                  <h3>Design</h3>
+  	                </div>
+  	                <div class="process-i-3">
+  	                  <p>This is when I roll up my sleeves and dive in. I’ll create an initial mood board based off of the images you’ve pinned and your questionnaire answers. With your go-ahead, I’ll head to the drawing board and start developing concepts. Once you’ve seen the initial designs, we’ll work collaboratively to tweak and perfect through two rounds of allotted revisions.</p>
+  	                </div>
+  	              </li>
+
+                  <li class="process-li">
+  	                <div class="process-i-1" id="ast-decor-lg">
+  	                  *
+  	                </div>
+  	                <div class="process-i-2">
+  	                  <h3>Development</h3>
+  	                </div>
+  	                <div class="process-i-3">
+  	                  <p>When the final design files are polished and prepped, we’ll set an official development start date and project timeline. We’ll begin developing your custom theme via a demo site on our private server to make sure your current site goes uninterrupted. Whew! Your second payment will also be due at this time.</p>
+  	                </div>
+  	              </li>
+
+                  <li class="process-li">
+  	                <div class="process-i-1" id="ast-decor-lg">
+  	                  *
+  	                </div>
+  	                <div class="process-i-2">
+  	                  <h3>Revise, Test, Launch</h3>
+  	                </div>
+  	                <div class="process-i-3">
+  	                  <p>Once your demo site is ready, it will be sent to your designer for proof corrections and design detail finessing before we pass it over to you for final approval. This is your chance to click around the site as if it were live to test the functionality on different browsers, provide feedback, ask questions, and prep the site for installation. After the demo site has been finalized, the final payment is due before we schedule an installation time and date that works well for both of us. During that time, we’ll put up a simple customized landing page on your site so all your fans can still find you! When the install is complete, we’ll email you a personalized video walkthrough of your shiny new site with directions on how to use it and make changes.</p>
+  	                </div>
+  	              </li>
+
+                  <li class="process-li">
+  	                <div class="process-i-1" id="ast-decor-lg">
+  	                  *
+  	                </div>
+  	                <div class="process-i-2">
+  	                  <h3>Post Launch Support</h3>
+  	                </div>
+  	                <div class="process-i-3">
+  	                  <p>We want you to get the most out of your site, which is why our Custom Themes are available for full maintenance and support services. We recommend all clients sign up for our Monthly Maintenance Plan, which is our way to guarantee that your site is in shape: backed up, up-to-date, malware-free. Or if you feel comfortable handling these tasks yourself, we also offer support and revisions on an as-needed basis.</p>
+  	                </div>
+  	              </li>
+
+                </ul>
+
+      </div> <!-- .gs-process -->
+
+      <div class="gs-faq">
+
+          <div class="grid-box grid-box-one">
+            <h6>Frequently Asked Questions</h6>
+          </div>
+
+          <div class="grid-box grid-box-two">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>How will you determine if my project is the right fit?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-three">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>How much will my website cost?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-four">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>What is the typical project timeline?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-five">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>What if I need to make future content changes to my site?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-six">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>Why do I need both a designer and a developer?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+      </div> <!-- .gs-faq -->
+
+
+    </div> <!-- .get-started -->
+
+	 </main><!-- #main -->
+ </div><!-- #primary -->
+
+
+
+<?php
+get_footer();
