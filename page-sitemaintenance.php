ce7bd7acba6b371a3ee64951e54bcc0eb7467b12
lizzie marie <elizabethmariewillett@gmail.com>
2018-05-17 15:40:38 -0700

    added site maintenance

---
+++ page-sitemaintenance.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * The template for displaying the get started with site maintenance page
+ *
+ *
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ *
+ * @package bst
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main get-started-m-main">
+
+    <div class="get-started">
+
+      <div class="gs-intro">
+
+              <div class="gs-intro-item-1">
+                <img src="http://localhost:8888/bandana-studio/wp-content/uploads/2018/05/ipad-test-bs.png" alt="keep scrolling" class="rotateimg100">
+              </div>
+
+              <div class="gs-intro-item-a">
+                <h5>B</h5>
+              </div>
+
+              <div class="gs-intro-item-2">
+                <h3>TLC for your site</h3>
+                <h2 class="text-shadow underline">Keep On<br></h2>
+                <h2 id="no-mt" class="text-shadow underline">shining</h2>
+                <p>Just like any investment, a beautiful site needs regular maintenance. I want you to get the most out of your site and the easiest way to do that is through my full monthly maintenance and support services. This is my way to guarantee that your site is running at full capacity: backed up, up-to-date, and malware-free. Taking this next step ensures that you are getting the most out of your investment!
+                <br>If you feel comfortable and prefer to handle your own site maintenance and backups, I also offer support and revisions at my hourly rate to any site created by Bandana Studio.</p>
+              </div>
+
+      </div> <!-- .gs-intro -->
+
+      <div class="gs-included">
+              <div class="gs-included-item-a">
+                <h5>*</h5>
+              </div>
+
+              <div class="gs-included-item-2">
+                <h3>Monthly Maintenance Plan</h3>
+                <h6> What's Included </h6>
+                <ul>
+                  <li>Priority support requests</li>
+                  <li>Daily security and malware scan</li>
+                  <li>Daily uptime monitoring</li>
+                  <li>Weekly Wordpress core updates</li>
+                  <li>Weekly WordPress plugin updates</li>
+                  <li>Weekly secure off-site backups</li>
+                  <li>Monthly maintenance report</li>
+                </ul>
+              </div>
+
+              <div class="gs-included-item-3">
+                <p>
+                  <a class="btn-dark pill" href="/bandana-studio/about/">contact us</a>
+                </p>
+              </div>
+      </div> <!-- .gs-included -->
+
+      <div class="gs-faq">
+
+          <div class="grid-box grid-box-one">
+            <h6>Frequently Asked Questions</h6>
+          </div>
+
+          <div class="grid-box grid-box-two">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>How will you determine if my project is the right fit?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-three">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>How much will my website cost?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-four">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>What is the typical project timeline?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-five">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>What if I need to make future content changes to my site?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+
+          <div class="grid-box grid-box-six">
+            <div class="grid-int">
+              <div class="grid-int-1">
+                <p>Why do I need both a designer and a developer?</p>
+                <h3>+</h3>
+              </div>
+            </div>
+          </div>
+      </div> <!-- .gs-faq -->
+
+    </div> <!-- .get-started -->
+
+	 </main><!-- #main -->
+ </div><!-- #primary -->
+
+
+
+<?php
+get_footer();
